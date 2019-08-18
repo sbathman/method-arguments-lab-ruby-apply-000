232968360624536f8f894d80fffb7d2ea26ece84
Samantha Bathman <bathman104@gmail.com>
2019-08-18 16:44:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,6 @@
 def introduction ($FILENAME)
   puts "Hi, my name is # {name}."
 end
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
